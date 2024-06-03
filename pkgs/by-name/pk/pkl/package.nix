0d260102450b32a26f28c56bcde29c37c8b2a4cd
Dan Boitnott <dboitnot@gmail.com>
2024-06-03 11:11:46 -0500

    pkl works on x86_64

--- pkgs/by-name/pk/pkl/package.nix
+++ pkgs/by-name/pk/pkl/package.nix
@@ -4,16 +4,30 @@
   fetchurl,
 }:
 
+let
+  src = fetchurl {
+    url = "https://github.com/apple/pkl/releases/download/0.25.3/pkl-alpine-linux-amd64";
+    hash = "sha256-+Qs/bIKyBuDZjfVoczFe19imLVXazoC/w3W8lIn9w+o=";
+    executable = true;
+  };
+in
 stdenv.mkDerivation {
   name = "pkl";
-  src = fetchurl {
-    url = "https://github.com/apple/pkl/releases/download/0.25.3/pkl-linux-amd64";
-    hash = "sha256-KU2WDCGzzjflmuJ9qMXJKIdjqE9uuYdyHCQVp8mRMps=";
-    executable = true;
-    downloadToTemp = true;
-    postFetch = ''
-      mkdir $out/bin
-      mv $downloadedFile $out/bin/pkl
-    '';
+  dontUnpack = true;
+  inherit src;
+
+  installPhase = ''
+    mkdir -p $out/bin $out/lib
+    cp ${src} $out/bin/pkl
+  '';
+
+  meta = with lib; {
+    description = "A configuration as code language with rich validation and tooling published by Apple.";
+    homepage = "https://pkl-lang.org/";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    license = licenses.asl20;
+    maintainers = with maintainers; [ dboitnot ];
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "pkl";
   };
 }
